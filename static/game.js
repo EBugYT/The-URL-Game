f301ce1c173c649f7142b4574f166059dd77e67e
EBugYT <89686363+EBugYT@users.noreply.github.com>
2023-10-01 16:10:43 +0200

    New mechanics

--- static/game.js
+++ static/game.js
@@ -1,3 +1,14 @@
-function gameLoop() {
-  hash = window.location.hash.slice(1);
+var hash = window.location.hash.slice(1);
+var hashtextinit = '😁#####----------------------------------------'.split('');
+if (hash == '') {
+  window.location.hash += '😁#####----------------------------------------';
 }
+function gameLoop() {
+  hash = window.location.hash
+    .slice(1)
+    .replace('%F0%9F%98%81', '😁')
+    .split('')
+    .splice(2, hash.length);
+}
+
+setTimeout(gameLoop(), 500);
